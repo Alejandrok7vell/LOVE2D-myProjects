215a12d3b62a9b1fd782809e31879c3bdec3c172
Alejandro Hernandez <alexk7v@gmail.com>
2022-05-19 11:11:21 -0500

    added more balls

--- physics/ball.lua
+++ physics/ball.lua
@@ -1,14 +1,18 @@
 function newBall()
    local ball = {}
 
-   function ball:load(x, y)
+   function ball:load(x, y, r, t)
       self.x, self.y = x or 100, y or 100
       self.xVel, self.yVel = 0, 0
-      self.impulse = 1200
-      self.r = 20
+      self.impulse = 2000
+      self.r = r or 20
       self.body = love.physics.newBody(world, self.x, self.y, "dynamic")
       self.shape = love.physics.newCircleShape(self.r)
-      self.fixture = love.physics.newFixture(self.body, self.shape, 1)
+      self.fixture = love.physics.newFixture(self.body, self.shape, 0.5)
+      self.team = t or 1
+      self.isTeam = true
+      self.isStop = true
+      self.ready = true
       -- ball bounce value
       self.fixture:setRestitution(1)
 
@@ -19,21 +23,37 @@ function newBall()
    end
 
    function ball:update()
-      -- checks if the x & y ball velocity are both equal 0
-      if ball.xVel == 0 and ball.yVel == 0 then
-
-         if love.mouse.isDown(1) and not mouseP then
-            mouseP = true
-            ball.body:setLinearVelocity(0, 0)
-         elseif not love.mouse.isDown(1) then
-            if mouseP then
-               ball:shoot()
-            end
-            mouseP = false
-         end
+      if self.current == self.team then
+         self.isTeam = true
+      else
+         self.isTeam = false
       end
 
-      ball.xVel, ball.yVel = ball.body:getLinearVelocity()
+      -- checks if the x & y ball velocity are both equal 0
+      if math.floor(self.xVel + 0.5) == 0 and math.floor(self.yVel + 0.5) == 0 then
+
+         self.isStop = true
+
+         if self.isTeam and self.ready then
+
+            if love.mouse.isDown(1) and not mouseP then
+               mouseP = true
+               self.body:setLinearVelocity(0, 0)
+            elseif not love.mouse.isDown(1) then
+               if mouseP then
+                  ball:shoot()
+                  self.isStop = false
+                  self.ready = false
+               end
+               mouseP = false
+            end
+         end
+      else
+         self.isStop = false
+         self.ready = false
+      end
+
+      self.xVel, self.yVel = self.body:getLinearVelocity()
    end
 
    function ball:draw()
@@ -62,7 +82,7 @@ function newBall()
       else
          impY = 0
       end
-      self.body:applyLinearImpulse(impX * ball:getForce(), impY * ball:getForce())
+      self.body:applyLinearImpulse(impX * self:getForce(), impY * self:getForce())
    end
 
    function ball:getForce()
@@ -77,40 +97,46 @@ function newBall()
    end
 
    function ball:drawLine()
-      local angulo = angle(mouseX, mouseY, self.body:getX(), self.body:getY())
-      local radio = self.radio
-      local hipotenusa = hipo(mouseX-self.body:getX(), mouseY-self.body:getY())
+      if self.isTeam and self.ready then
+         local angulo = angle(mouseX, mouseY, self.body:getX(), self.body:getY())
+         local radio = self.radio
+         local hipotenusa = hipo(mouseX-self.body:getX(), mouseY-self.body:getY())
 
-      -- looks mouse distance value to radio value
-      if hipotenusa > radio then hipotenusa = radio end
+         -- looks mouse distance value to radio value
+         if hipotenusa > radio then hipotenusa = radio end
 
-      -- get the C values
-      local xw = math.sin(math.rad(angulo))*hipotenusa
-      local yh = math.pow(hipotenusa, 2) - math.pow(xw, 2)
+         -- get the C values
+         local xw = math.sin(math.rad(angulo))*hipotenusa
+         local yh = math.pow(hipotenusa, 2) - math.pow(xw, 2)
 
-      -- converts yh to positive value for work with math.sqrt(x)
-      if yh < 0 then yh = - yh end
+         -- converts yh to positive value for work with math.sqrt(x)
+         if yh < 0 then yh = - yh end
 
-      -- get the CO long
-      yh = math.sqrt(math.floor(yh + 0.5))
+         -- get the CO long
+         yh = math.sqrt(math.floor(yh + 0.5))
 
-      -- looks the values to radio value
-      if xw > radio then xw = radio end
-      if yh > radio then yh = radio end
+         -- looks the values to radio value
+         if xw > radio then xw = radio end
+         if yh > radio then yh = radio end
 
-      if mouseX < self.body:getX() then xw = -xw end
-      if mouseY < self.body:getY() then yh = -yh end
+         if mouseX < self.body:getX() then xw = -xw end
+         if mouseY < self.body:getY() then yh = -yh end
 
-      if love.mouse.isDown(1) then
-         love.graphics.setColor(0, 0, 0)
+         if love.mouse.isDown(1) then
+            love.graphics.setColor(0, 0, 0)
 
-         love.graphics.line(
-            math.floor(self.body:getX() + xw),
-            math.floor(self.body:getY() + yh),
-            self.body:getX(),
-            self.body:getY()
-         )
+            love.graphics.line(
+               math.floor(self.body:getX() + xw),
+               math.floor(self.body:getY() + yh),
+               self.body:getX(),
+               self.body:getY()
+            )
+         end
       end
+   end
+
+   function ball:setCurrentTeam(t)
+      self.current = t
    end
 
    return ball
--- physics/main.lua
+++ physics/main.lua
@@ -14,12 +14,22 @@ function love.load()
    love.physics.setMeter(64)
    world = love.physics.newWorld(0, 0)
 
+   team = 2
+
    balls = {}
-   for i = 1, 2, 1 do
+   ballsLength = 3
+   ballsStoped = 0
+   for i = 1, ballsLength, 1 do
       table.insert(balls, newBall())
    end
 
-   balls[1]:load(200, 200)
+   balls[1]:load(200, 200, 50)
+   balls[2]:load(400, 300, 50, 2)
+   balls[3]:load(500, 100, 50, 2)
+
+   for key in pairs(balls) do
+      balls[key]:setCurrentTeam(team)
+   end
    paredes:load()
 end
 
@@ -30,6 +40,8 @@ function love.update(dt)
       balls[key]:update()
    end
 
+   areBallsStoped()
+
    mouseX, mouseY = love.mouse.getPosition()
 end
 
@@ -37,5 +49,19 @@ function love.draw()
    for key in pairs(balls) do
       balls[key]:draw()
       balls[key]:drawLine()
+   end
+end
+
+function areBallsStoped()
+   if balls[1].isStop and
+      balls[2].isStop and
+      balls[3].isStop then
+         for i = 1, ballsLength, 1 do
+            balls[i].ready = true
+         end
+      else
+         for i = 1, ballsLength, 1 do
+            balls[i].ready = false
+         end
    end
 end
