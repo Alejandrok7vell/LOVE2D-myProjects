c20b0cb333d4bdd7c35b6490c9da9b0ad9066829
Alejandrok7vell <alexk7v@gmail.com>
2022-07-13 19:13:48 -0500

    commit

--- particle/main.lua
+++ particle/main.lua
@@ -1,34 +1,46 @@
 function love.load()
+	math.randomseed(os.time())
+	mouseX, mouseY = love.mouse:getPosition()
 	local img = love.graphics.newImage('1.png')
 	particles = {
-		x = love.graphics.getWidth() / 2,
-		y = love.graphics.getHeight() / 2
+		x = mouseX,
+		y = mouseY
 	}
 
 	angle = 0
 
 	psystem = love.graphics.newParticleSystem(img, 100)
-	psystem:setEmissionArea("uniform", 10, 10, angle, false)
+	psystem:setEmissionArea("uniform", 5, 5, 0, true)
 	psystem:setParticleLifetime(1, 1) -- Particles live at least 2s and at most 5s.
-	psystem:setEmissionRate(50)
-	psystem:setSpeed(100, 300)
+	psystem:setSpeed(100, 200)
+	psystem:setEmissionRate(70)
 	psystem:setColors(1, 1, 1, 1, 1, 1, 1, 0) -- Fade to transparency.
 
 	mouseP = false
 end
 
 function love.update(dt)
+	color = {
+		math.random(),
+		math.random(),
+		math.random()
+	}
+	if love.keyboard.isDown("escape") then love.event.quit() end
 	psystem:update(dt)
+	mouseX, mouseY = love.mouse:getPosition()
+	--[[
 	if love.mouse.isDown(1) and not mouseP then
 		mouseP = true
 		angle = angle + 0.785398
 		psystem:setDirection(angle)
 	elseif not love.mouse.isDown(1) and mouseP then
 		mouseP = false
-	end
+	end]]
+	psystem:setPosition(mouseX, mouseY)
 end
 
 function love.draw()
 	-- Draw the particle system at the center of the game window.
-	love.graphics.draw(psystem, particles.x, particles.y)
+	love.graphics.setColor(color)
+	love.graphics.draw(psystem)
 end
